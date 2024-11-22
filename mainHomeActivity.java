c7c87ad0ff77dda9d9ed729d90a97de414a45e7f
Bhakti-09 <bhaktipatil724@gmail.com>
2024-11-22 15:50:35 +0530

    Add files via upload

--- mainHomeActivity.java
+++ mainHomeActivity.java
@@ -1,9 +1,16 @@
 package com.example.a1stexample;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.nfc.cardemulation.CardEmulation;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -19,6 +26,27 @@ public class mainHomeActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        SharedPreferences sharedPreferences=getSharedPreferences("shared_pref", Context.MODE_PRIVATE);
+        String username=sharedPreferences.getString("username","").toString();
+        Toast.makeText(getApplicationContext(),"Welcome "+username,Toast.LENGTH_SHORT).show();
+
+        CardView exit=findViewById(R.id.cardExit);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor=sharedPreferences.edit();
+                editor.clear();
+                editor.apply();
+                startActivity(new Intent(mainHomeActivity.this,LoginActivity.class));
+            }
+        });
+        CardView addroom=findViewById(R.id.cardRooms);
+        addroom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(mainHomeActivity.this,AddRoomActivity.class));
+            }
         });
     }
 }
